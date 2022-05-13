19a53258b48d59fe679e5a8c4dac7888a37d7b1b
kibet kilel <amos.rotich@strathmore.edu>
2022-05-13 23:54:48 +0300

    update

--- auth/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class UserProfile(models.Model):
-    address = models.CharField(max_length=512)
-    phone = models.CharField(max_length=40)
--- pizza/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-from .views import index
-
-urlpatterns = [
-    path('<int:pid>', index, name='pizza'),
-    ]

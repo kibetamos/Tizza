d780e6e2005310cbcfc29de00edf55d4d42491ec
Amos <33954664+kibetamos@users.noreply.github.com>
2023-08-31 13:50:04 +0000

    updtaed pizza urls

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,17 +1,11 @@
-from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth.views import LoginView, LogoutView
 
-# from pizza.routers import router as pizza_router
+from .views import index, GetTenPizzasView
+
+
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('pizzas/', include('pizza.urls')),
-
-    # path('user/', include('user.urls')),
-    # path('login/', LoginView.as_view()),
-    # path('logout/', LogoutView.as_view()),
-
-    # path('', include(pizza_router.urls)),
+    path('<int:pid>', index),
+    path('ten', GetTenPizzasView.as_view()),
 ]

4e7bdb1f1cc010f1572d3445b7297aaa8cb18c52
kibetamos <amos.rotich@strathmore.edu>
2023-09-25 17:50:24 +0300

    Update: added user profile

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db import User
 
 # Create your models here.
+from django.db import models
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    

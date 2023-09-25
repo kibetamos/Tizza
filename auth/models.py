d63db5da6af469a974122517665f6001f298d12b
kibetamos <amos.rotich@strathmore.edu>
2023-09-25 17:52:36 +0300

    Update: added user profile

--- auth/models.py
+++ auth/models.py
@@ -7,4 +7,9 @@ from django.db import models
 class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
     

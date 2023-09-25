aba244e9ff84135aab2dfb1d55d24d24e22341c5
kibetamos <amos.rotich@strathmore.edu>
2023-09-25 17:43:15 +0300

    Update: added auth ap

--- pizza/urls.py
+++ pizza/urls.py
@@ -2,9 +2,6 @@ from django.urls import include, path
 
 from .views import index, GetTenPizzasView
 
-
-
-
 urlpatterns = [
     path('<int:pid>', index),
     path('ten', GetTenPizzasView.as_view()),
